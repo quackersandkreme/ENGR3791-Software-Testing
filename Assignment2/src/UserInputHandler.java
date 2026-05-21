533130f4517e5bef989fae948c80b3c999b3ad91
Dante <you@example.com>
2026-05-21 18:19:09 +0930

    Added banner

--- Assignment2/src/UserInputHandler.java
+++ Assignment2/src/UserInputHandler.java
@@ -1,19 +1,41 @@
 import java.util.Scanner;
 
 public class UserInputHandler {
+
+    // Colours used in banner
+    public static final String RESET = "\u001B[0m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String PURPLE = "\u001B[35m";
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // Opening message for user
-        System.out.println("==================================");
-        System.out.println("   TIMETABLE CLI");
-        System.out.println("   Type 'help' for commands");
-        System.out.println("   Type 'exit' to quit");
-        System.out.println("==================================");
+        // Opening banner
+        System.out.println(PURPLE + """
+  _______ _                _        _     _         _____ _      _____ 
+ |__   __(_)              | |      | |   | |       / ____| |    |_   _|
+    | |   _ _ __ ___   ___| |_ __ _| |__ | | ___  | |    | |      | |  
+    | |  | | '_ ` _ \\ / _ \\ __/ _` | '_ \\| |/ _ \\ | |    | |      | |  
+    | |  | | | | | | |  __/ || (_| | |_) | |  __/ | |____| |____ _| |_ 
+    |_|  |_|_| |_| |_|\\___|\\__\\__,_|_.__/|_|\\___|  \\_____|______|_____|
+""" + RESET);
+
+        System.out.println(CYAN + "        TIMETABLE OPTIMISATION CLI" + RESET);
+        System.out.println();
+
+        System.out.println(GREEN + "Commands:" + RESET);
+        System.out.println("  help              - Show all commands");
+        System.out.println("  help <command>    - Command details");
+        System.out.println("  exit              - Quit program");
+
+        System.out.println();
+
+        System.out.println(YELLOW + "Type a command and press Enter" + RESET);
+        System.out.println();
 
         while (in.hasNextLine()) {
-            // CLI prompt
-            System.out.print("CLI > ");
             String line = in.nextLine();
             String[] input = line.split(" ");
 
