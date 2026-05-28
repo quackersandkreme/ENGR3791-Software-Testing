c6ad8108240d86e0e68df0a68b072349f932024f
quackersnkreme <charliefielding160@gmail.com>
2026-05-28 15:47:54 +0930

    Setting up test environment part 2

--- Assignment2/src/test/java/ApplicationTest.java
+++ Assignment2/src/test/java/ApplicationTest.java
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest {
 
-    
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    public void setUp() {
+        System.setOut(new PrintStream(output));
+    }
 
 }
