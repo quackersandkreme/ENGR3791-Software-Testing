576ed6d0cab45cb426dde19ae9b576aeb7481c3d
quackersnkreme <charliefielding160@gmail.com>
2026-05-30 14:05:29 +0930

    setup for testing env

--- Assignment2/src/test/java/UserInputHandlerTest.java
+++ Assignment2/src/test/java/UserInputHandlerTest.java
@@ -3,23 +3,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputHandlerTest {
 
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
+    private final InputStream originalInputStream = System.in;
+    private final PrintStream originalOutputStream = System.out;
+    private final ByteArrayOutputStream captureOutputStream = new ByteArrayOutputStream();
 
     @BeforeEach
     public void setUp() {
-        System.setOut(new PrintStream(output));
+        // Reassigns the "standard" output stream to "captureOutputStream".
+        System.setOut(new PrintStream(captureOutputStream));
     }
 
     @AfterEach
     public void tearDown() {
-        System.setOut(originalOut);
+        // Reassigns the "standard" input stream back to what was originally stored in "originalInputStream".
+        System.setIn(originalInputStream);
+        // Reassigns the "standard" output stream back to what was originally stored in "originalOutputStream".
+        System.setOut(originalOutputStream);
     }
 
 }
